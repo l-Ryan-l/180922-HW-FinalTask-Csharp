39dd9b162d5b14ae07b67b96360cca6219acdc90
Ryan <llryanll@yandex.ru>
2022-09-18 20:27:11 +0300

    task solved

--- final_task/Program.cs
+++ final_task/Program.cs
@@ -7,3 +7,46 @@
 ["1234", "1257", "2", "computer science"] -> ["2"]
 ["Russia", "Denmark", "Kazan"] -> [] 
 */
+
+
+string[] arrayFirst = new string[5] { "hello", "2", "world", ":-)", "1234" };
+string[] arrayFinal = new string[arrayFirst.Length];
+
+void FillArrayFinal(string[] arrayFirst, string[] arrayFinal)
+{
+    int count = 0;
+    for (int i = 0; i < arrayFirst.Length; i++)
+    {
+        if (arrayFirst[i].Length <= 3)
+        {
+            arrayFinal[count] = arrayFirst[i];
+            count++;
+        }
+        else
+        {
+            arrayFinal[count] = " ";
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+FillArrayFinal(arrayFirst, arrayFinal);
+
+Console.WriteLine("Исходный массив: ");
+PrintArray(arrayFirst);
+Console.WriteLine();
+
+Console.WriteLine("Результирующий массив: ");
+PrintArray(arrayFinal);
+
+
